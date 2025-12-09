a03502b7f1ed880d2957ac39f073b57e80e7b65c
shakirjon803-cell <shakirjon803@gmail.com>
2025-12-09 21:14:39 +0200

    Added package-lock.json

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -4,11 +4,49 @@ import { useLocation, useNavigate } from 'react-router-dom';
 
 function NellXLogo() {
   return (
-    <img
-      src="/gemini_generated_image_4g9jw74g9jw74g9j.png"
-      alt="NellX"
-      className="h-9 w-auto"
-    />
+    <svg
+      width="120"
+      height="36"
+      viewBox="0 0 120 36"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      className="select-none"
+      aria-label="NellX"
+    >
+      {/* N */}
+      <path
+        d="M4 28V8h3.2l8.4 13.6V8h3.2v20h-3.2L7.2 14.4V28H4z"
+        fill="#111827"
+      />
+      {/* e */}
+      <path
+        d="M24.5 21.2c0 .4.1.6.1.8h-8.8c.2 1.2.7 2.1 1.5 2.7.8.6 1.8.9 3 .9 1.6 0 2.9-.5 3.9-1.6l1.8 2c-.7.8-1.5 1.4-2.5 1.8-1 .4-2.1.6-3.4.6-1.5 0-2.9-.3-4-1-1.2-.6-2.1-1.5-2.7-2.7-.6-1.1-1-2.4-1-3.9 0-1.4.3-2.7.9-3.9.6-1.1 1.5-2 2.6-2.7 1.1-.6 2.4-1 3.8-1 1.4 0 2.6.3 3.7 1 1.1.6 1.9 1.5 2.5 2.6.6 1.1.9 2.4.9 3.9v.5zm-8.8-1.4h6.2c-.1-1.1-.5-2-1.2-2.6-.7-.6-1.5-.9-2.5-.9s-1.8.3-2.5.9c-.6.6-1 1.5-1 2.6z"
+        fill="#111827"
+      />
+      {/* l */}
+      <path
+        d="M27 28V6h3v22h-3z"
+        fill="#111827"
+      />
+      {/* l */}
+      <path
+        d="M33 28V6h3v22h-3z"
+        fill="#111827"
+      />
+      {/* X - stylized with gradient */}
+      <defs>
+        <linearGradient id="xGradient" x1="40" y1="8" x2="60" y2="28" gradientUnits="userSpaceOnUse">
+          <stop offset="0%" stopColor="#3B82F6" />
+          <stop offset="100%" stopColor="#1D4ED8" />
+        </linearGradient>
+      </defs>
+      <path
+        d="M40 8h4.2l5.8 8.8L55.8 8H60l-8 11.5L60.2 28H56l-6-8.5-6 8.5h-4.2l8.2-8.5L40 8z"
+        fill="url(#xGradient)"
+      />
+      {/* Decorative dot */}
+      <circle cx="67" cy="24" r="3" fill="#3B82F6" />
+    </svg>
   );
 }
 
