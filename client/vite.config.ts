4975fd6177dcb21fde88fae474499cf43a7dac2d
shakirjon803-cell <shakirjon803@gmail.com>
2025-12-09 21:08:59 +0200

    Updated .env

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -10,6 +10,10 @@ export default defineConfig({
             "@": path.resolve(__dirname, "./src"),
         },
     },
+    build: {
+        outDir: '../dist',
+        emptyOutDir: true,
+    },
     server: {
         proxy: {
             '/api': {
